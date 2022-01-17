d3c5db52e49f2734ef6db51eb0279af192e9a173
leandro <leandropinheiroalves@gmail.com>
2022-01-17 14:24:47 -0300

    Extraida configuração de SECRET_KEY
    close #12

--- cdjango/settings.py
+++ cdjango/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-@m@nhswt_b-*cfp)c2rb(_p#doozckcu^ta67-&9xaywovuav6'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
