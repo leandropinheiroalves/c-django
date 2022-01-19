2819e65843e17b731a9950dd4c6e82f69f399563
leandro <leandropinheiroalves@gmail.com>
2022-01-19 15:16:35 -0300

    Instalada Django Debug Toolbar
    close #27

--- cdjango/base/views.py
+++ cdjango/base/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
-# from django.shortcuts import render
-
-# Create your views here.
 
 
 def home(request):
-    return HttpResponse('Olá Django')
+    # return HttpResponse('Olá Django')
+    return HttpResponse('<html><body>Olá Django</body></html>')
--- cdjango/settings.py
+++ cdjango/settings.py
@@ -73,6 +73,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cdjango.wsgi.application'
 
+# Configuração Django Debug Toolbar
+
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
