7f278643f3c498337f1dffcd61500e5f640a6562
leandro <leandropinheiroalves@gmail.com>
2022-01-30 18:45:07 -0300

    Criada Página de Detalhamento de Aula
    close #71

--- cdjango/modulos/facade.py
+++ cdjango/modulos/facade.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from cdjango.modulos.models import Modulo
+from cdjango.modulos.models import Modulo, Aula
 
 
 def listar_modulos_ordandos() -> List[Modulo]:
@@ -17,3 +17,7 @@ def encontrar_modulo(slug: str) -> Modulo:
 
 def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
     return list(modulo.aula_set.order_by('order').all())
+
+
+def encontrar_aula(slug):
+    return Aula.objects.get(slug=slug)
