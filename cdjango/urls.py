37143134cc86fe11edba4f49cc1e5784b6a25042
leandro <leandropinheiroalves@gmail.com>
2022-01-31 07:27:56 -0300

    Criado Formulário de Login
    close #93

--- cdjango/urls.py
+++ cdjango/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contas/', include('django.contrib.auth.urls')),
     path('', include('cdjango.base.urls')),
     path('aperitivos/', include('cdjango.aperitivos.urls')),
     path('modulos/', include('cdjango.modulos.urls')),
