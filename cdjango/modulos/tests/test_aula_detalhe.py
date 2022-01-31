78b81be160492a878bb8bddedeb4dbdfbff0b02d
leandro <leandropinheiroalves@gmail.com>
2022-01-30 21:11:47 -0300

    Corrigido ineficiência do Breadcrumb
    close #85

--- cdjango/modulos/tests/test_aula_detalhe.py
+++ cdjango/modulos/tests/test_aula_detalhe.py
@@ -27,7 +27,7 @@ def test_titulo(resp, aula: Aula):
 
 
 def test_vimeo(resp, aula: Aula):
-    assert_contains(resp, f'src="https://player.vimeo.com/video/{ aula.vimeo_id }"')
+    assert_contains(resp, f'src="https://player.vimeo.com/video/{aula.vimeo_id}"')
 
 
 def test_modulo_breadcrumb(resp, modulo: Modulo):
